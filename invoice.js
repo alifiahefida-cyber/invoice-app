f71b32cb26478314144dce10b14c9a99d5e2e229
alifiahefida-cyber <alifiahefida@gmail.com>
2025-12-16 22:14:41 +0800

    Update invoice.js

--- invoice.js
+++ invoice.js
@@ -1,4 +1,5 @@
-alert 'fix'
+alert("fix");
+
 // =====================================================
 // invoice.js — FIX FINAL (PREVIEW AMAN)
 // =====================================================
@@ -241,6 +242,7 @@ Mohon lakukan pembayaran maksimal pukul 17.00 WIB H-1 pengiriman.
 };
 
 })();
+
 
 
 
